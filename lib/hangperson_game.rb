16e1d20231f493e0ae32f0ef0263a4764c4a7d0d
DuffyTJ89 <tomduffy89@gmail.com>
2019-02-07 13:16:00 +0000

    fails in guessing, need to fix

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -17,6 +17,7 @@ class HangpersonGame
     @wrong_guesses = ''
   end
 
+
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
   #  => "cooking"   <-- some random word
@@ -28,5 +29,34 @@ class HangpersonGame
       return http.post(uri, "").body
     }
   end
+  
+  def guessing(guess)
+    
+    if (guess=='' || guess== NIL) #check if an empty word is entered if so disallow it
+      raise ArgumentError, "No word entered"
+    end
+    
+    if (guess.match(/[^A-Za-z]/)) # check if the user has entered a number, if so disallow it. 
+      raise ArgumentError, "No numbers, only letters"
+    end
+    
+    guess=guess.downcase() #change all the letters to lowercase
+    lists=@word.split("") #split words 
+    lists.each do |list| # go over the list
+      if(guess==list) # the guess is in the list
+        @wrong_guesses+=""
+        if(!guesses.include?guess) 
+          @guesses+=guess
+          return true 
+        end
+      
+        return false
+  
+      end #end of list check
+      
+      
+    end
+    
+  end #end guessing
 
 end
